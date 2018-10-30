14c9ad7980d023adf1c5e7d380dd865f4fa6d394
caiguochao <419831717@qq.com>
2018-10-30 15:12:31 +0800

    update

---
+++ gitpush.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+git add ~/.vim
+git add ~/.vimrc
+git add ~/reload.sh
+git add ~/run.sh
+git add ~/stop.sh
+git add ~/svnupdate.sh
+git add ~/telclient.ex
+git add ~/gitpush.sh
+git commit -m "update"
+git push origin master
