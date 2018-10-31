bcdcc7c866a72b1b6efa17aa7f171fc659c1f9a4
caiguochao <419831717@qq.com>
2018-10-31 16:14:36 +0800

    update

--- telclient.ex
+++ telclient.ex
@@ -1,6 +1,8 @@
 #!/usr/bin/expect
 set timeout 10
-spawn telnet 127.0.0.1 5600
+set port [lindex $argv 0]
+set port [expr {$port + 5600}]
+spawn telnet 127.0.0.1 $port
 send "from common.EntityManager import EntityManager as e\r"
 sleep 0.01
 send "for key in sorted(e._entities.values()):\r"
