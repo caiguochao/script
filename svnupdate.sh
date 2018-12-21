70b811d93fee4835bf7da4c61af2d8e4f850a1f3
caiguochao <419831717@qq.com>
2018-12-21 16:27:35 +0800

    update

--- svnupdate.sh
+++ svnupdate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd ~/trunk/Program/Script/Python
-find . -name "*.pyo" | xargs rm -f
 cd ~/trunk
-svn update
+svn up
 cd ~/trunk/Program/Server/operations
 bash reload.sh -o
--- telclient.ex
+++ telclient.ex
@@ -4,24 +4,19 @@ set port [lindex $argv 0]
 set port [expr {$port + 5600}]
 spawn telnet 127.0.0.1 $port
 send "from common.EntityManager import EntityManager as e\r"
-sleep 0.05
-send "for key in sorted(e._entities.values()):\r"
-sleep 0.05
-send "    if 'SpaceStub' in key.__str__(): spaceStub = key\r"
-sleep 0.05
-send "    if 'SphereStub' in key.__str__(): sphereStub = key\r"
-sleep 0.05
-send "    if 'TeamStub' in key.__str__(): teamStub = key\r"
-sleep 0.05
-send "    if 'StatStub' in key.__str__(): statStub = key\r"
-sleep 0.05
-send "    if 'RollStub' in key.__str__(): rollStub = key\r"
-sleep 0.05
-send "    if 'GlobalEventCallback' in key.__str__(): callBack = key\r"
-sleep 0.05
-send "    if 'Channel' in key.__str__(): channel = key\r"
-sleep 0.05
-send "    print key\r\r\r"
-sleep 1
-send "p=e._entities\['XATnNAY5BnO1Tgjl'\]\r"
+expect "from"
+sleep 0.01
+send "e = e._entities\r"
+expect "entities"
+sleep 0.01
+send "p=e\['XATnNAY5BnO1Tgjl'\]\r"
+expect "p"
+sleep 0.01
+send "for key in sorted(e.values()):\r"
+expect "sorted"
+sleep 0.01
+send "    print key\r"
+expect "key"
+sleep 0.01
+send "\r"
 interact
