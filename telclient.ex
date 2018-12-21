0065f396e4e3ed51f2ddb053fa2f94349109e6b4
caiguochao <419831717@qq.com>
2018-12-21 16:29:14 +0800

    update

--- telclient.ex
+++ telclient.ex
@@ -4,19 +4,12 @@ set port [lindex $argv 0]
 set port [expr {$port + 5600}]
 spawn telnet 127.0.0.1 $port
 send "from common.EntityManager import EntityManager as e\r"
-expect "from"
 sleep 0.01
 send "e = e._entities\r"
-expect "entities"
 sleep 0.01
 send "p=e\['XATnNAY5BnO1Tgjl'\]\r"
-expect "p"
 sleep 0.01
 send "for key in sorted(e.values()):\r"
-expect "sorted"
 sleep 0.01
-send "    print key\r"
-expect "key"
-sleep 0.01
-send "\r"
+send "    print key\r\r"
 interact
